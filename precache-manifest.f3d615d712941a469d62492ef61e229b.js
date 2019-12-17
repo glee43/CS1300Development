ea78e4d1df595b0675fec857d934e54260fabdc8
George <george_lee@brown.edu>
2019-12-17 14:09:59 -0800

    Updates

---
+++ precache-manifest.f3d615d712941a469d62492ef61e229b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1bcf61ec55036b78a30ffe0803b4cd",
+    "url": "/CS1300Development/index.html"
+  },
+  {
+    "revision": "81714dd60771b43b3824",
+    "url": "/CS1300Development/static/css/main.1a3d0d8a.chunk.css"
+  },
+  {
+    "revision": "5dc0426b496a321cdb9c",
+    "url": "/CS1300Development/static/js/2.f7bd3b2f.chunk.js"
+  },
+  {
+    "revision": "75ad5eec72c062dc6e25286dd45be92a",
+    "url": "/CS1300Development/static/js/2.f7bd3b2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81714dd60771b43b3824",
+    "url": "/CS1300Development/static/js/main.5a0bd8b1.chunk.js"
+  },
+  {
+    "revision": "a6d985b4bcff92c7d7f7",
+    "url": "/CS1300Development/static/js/runtime-main.68a1050e.js"
+  },
+  {
+    "revision": "90acc7d8094d4736a1208dceab20db88",
+    "url": "/CS1300Development/static/media/donkeyKong.90acc7d8.jpg"
+  },
+  {
+    "revision": "1ccba2ff888b6037bae70d5ae565f764",
+    "url": "/CS1300Development/static/media/fox.1ccba2ff.jpg"
+  },
+  {
+    "revision": "a0fe9ba87f12aad11257f88c266ab1ba",
+    "url": "/CS1300Development/static/media/link.a0fe9ba8.jpg"
+  },
+  {
+    "revision": "63263e51f85c6a9a12c7938c5eadccf3",
+    "url": "/CS1300Development/static/media/mario.63263e51.jpg"
+  }
+]);
